9abd36ef42f5e394d1f75424e54d2939f7a57998
Luc Weytingh <lucweytingh321@gmail.com>
2020-05-13 18:11:04 +0200

    new observations are now saved in the beliefs

--- agents/builder.py
+++ agents/builder.py
@@ -2,12 +2,12 @@ from .helpers import Agent
 
 
 class Builder(Agent):
-    def get_action(self, new_obstacle, new_empty, new_agents):
+    def get_action(self):
 
         # do some reasoning with new_obstacle, new_empty, new_agents and self.graph
         
         # i want to navigate to (5,10)
-        action = self.nav_to((-2, -12), new_obstacle + new_empty + new_agents)
+        action = self.nav_to((-2, -12))
         
         # check if arrived or no path is possible
         if not action:
